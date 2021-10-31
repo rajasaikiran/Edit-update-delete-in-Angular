65f93733e0ecd34fb18f788d7ecf2feb5c92fad3
Rajasaikiran Vemula <rvemula@ariqt.com>
2021-10-31 22:31:16 +0530

    Edit-Update-Delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CARS } from './cars';
 
+interface Car {
+  id: number;
+  brand: string;
+  model: string;
+  year: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Edit-Update';
+  cars: Car[] = CARS;
+  car!: FormGroup;
+
+  editCar(car: Car) {
+    this.car = new FormGroup({
+      id: new FormControl(car.id),
+      brand: new FormControl(car.brand),
+      model: new FormControl(car.model),
+      year: new FormControl(car.year),
+    });
+  }
+
+  DeleteCar(i: any) {
+    this.cars.splice(i, 1);
+  }
+
+  save() {
+    let index = this.cars.findIndex((car) => car.id === this.car.value.id);
+    this.cars[index] = this.car.value;
+    this.car.reset()
+  }
+
+  cancel() {
+    this.car == null;
+  }
 }
---
+++ src/app/cars.ts
@@ -0,0 +1,32 @@
+export const CARS = [
+  {
+    id: 0,
+    brand: 'Audi',
+    model: 'A4',
+    year: 2018,
+  },
+  {
+    id: 1,
+    brand: 'BMW',
+    model: '3 Series',
+    year: 2015,
+  },
+  {
+    id: 2,
+    brand: 'Mercedes-Benz',
+    model: 'C Klasse',
+    year: 2016,
+  },
+  {
+    id: 3,
+    brand: 'Volva',
+    model: 'C-class',
+    year: 2021,
+  },
+  {
+    id: 4,
+    brand: 'maruthi',
+    model: 'Breeza',
+    year: 2021,
+  },
+];
